44781770db550704ec6167d9c502994e0a96445d
Denis <Django_155@tut.by>
2022-07-11 15:45:54 +0300

    3 lesson model & admin

--- car/auto/admin.py
+++ car/auto/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'costs', 'description')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description')
+    list_editable = ('costs',)
+    list_filter = ('title','costs','description')
+    list_per_page = 3 # пагинация
+    ordering = ('title','costs')
+
+admin.site.register(Car,CarsAdmin)
--- car/auto/models.py
+++ car/auto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    title = models.CharField(max_length=20,verbose_name="название")
+    speed = models.FloatField(verbose_name="скорость")
+    costs = models.IntegerField(verbose_name="стоимость")
+    description = models.TextField(verbose_name="описание")
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Машины'
+        verbose_name_plural = 'Машины'
+        # ordering = ['id', 'title']
